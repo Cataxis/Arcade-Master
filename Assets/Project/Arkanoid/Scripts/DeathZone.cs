58fb1d596ffc3842f3ca1a92a6d8f8d1d3af0572
Diego Nachon <diego211@live.com.mx>
2023-08-04 21:55:15 -0600

    fix deathZone gizmo

--- Assets/Project/Arkanoid/Scripts/DeathZone.cs
+++ Assets/Project/Arkanoid/Scripts/DeathZone.cs
@@ -7,20 +7,14 @@ public class DeathZone : MonoBehaviour, IDamagable
 {
     private Collider2D zoneCollider;
 
+
+
     void OnDrawGizmos()
     {
-        if (zoneCollider == null)
-        {
-            zoneCollider = GetComponent<Collider2D>();
-        }
+        if (zoneCollider == null) zoneCollider = GetComponent<Collider2D>();
 
         Gizmos.color = Color.red;
         Gizmos.DrawCube(zoneCollider.bounds.center, zoneCollider.bounds.size);
-
-        GUIStyle style = new GUIStyle();
-        style.normal.textColor = Color.red;
-        style.fontSize = 14;
-        Handles.Label(zoneCollider.bounds.center, "Death Zone", style);
     }
 
     public void Damage()
